3587f3dbb42d695bd3f4d1519d76e84af6b469fc
Shaaban <shabanhamdy94@gmail.com>
2024-03-23 13:37:33 +0200

    first commit

--- db/connection_db.js
+++ db/connection_db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 
-const ConnectionDB =async ()=>{
+const Connection_db =async ()=>{
         return await mongoose.connect(process.env.CONNECTION_DB)
         // return await mongoose.connect("mongodb+srv://cycle41:cycle41@cluster0.e09gpvw.mongodb.net/cycle41ECommerce")
         .then(res=>console.log("ConnectionDB is Running........"))
@@ -14,4 +14,4 @@ const ConnectionDB =async ()=>{
 
 
 
-export default ConnectionDB
+export default Connection_db
--- src/utils/AppInit.js
+++ src/utils/AppInit.js
@@ -3,7 +3,7 @@
 import cors from 'cors'
 import path from "path";
 import { fileURLToPath } from "url";
-import ConnectionDB from '../../DB/ConnectionDB.js';
+import Connection_db from '../../db/connection_db.js';
 const __dirName = path.dirname(fileURLToPath(import.meta.url))
 // import  globalErrorHandling  from './error.handling.js';
 
@@ -13,7 +13,7 @@ const __dirName = path.dirname(fileURLToPath(import.meta.url))
 const AppInit = (app, express) => {
     app.use(express.json({}))
     app.use(cors())
-    ConnectionDB()
+    Connection_db()
     // ===================================================================
     // app.use("/uploads/images", express.static(path.join(__dirName, "../uploads/images")))
     // app.use("/user", appRouters.userRouter)
