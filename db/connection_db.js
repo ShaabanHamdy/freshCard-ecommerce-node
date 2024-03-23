62f4612b9732676df3c9580e8c10ef9b30d5e3e1
Shaaban <shabanhamdy94@gmail.com>
2024-03-23 20:09:23 +0200

    first commit

--- db/connection_db.js
+++ db/connection_db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 
 
 const Connection_db =async ()=>{
-        return await mongoose.connect(process.env.CONNECTION_DB)
-        // return await mongoose.connect("mongodb+srv://cycle41:cycle41@cluster0.e09gpvw.mongodb.net/cycle41ECommerce")
+        // return await mongoose.connect(process.env.CONNECTION_DB)
+        return await mongoose.connect("mongodb+srv://cycle41:cycle41@cluster0.e09gpvw.mongodb.net/cycle41ECommerce")
         .then(res=>console.log("ConnectionDB is Running........"))
         .catch(err=>console.log({message:"fail in ConnectionDB" , err}))
 }
