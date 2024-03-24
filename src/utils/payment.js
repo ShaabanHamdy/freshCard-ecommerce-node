a8e8f379499b34c08d658ad113e331bd34ef4487
Shaaban <shabanhamdy94@gmail.com>
2024-03-24 22:18:41 +0200

    first commit

--- src/utils/payment.js
+++ src/utils/payment.js
@@ -2,13 +2,14 @@ import Stripe from "stripe";
 
 
 async function payment({
-    stripe = new Stripe(process.env.STRIPE_KEY),
+    // stripe = new Stripe(process.env.STRIPE_KEY),
+    stripe = new Stripe("sk_test_51N9oE8E8FB2eaC6mX97vBXjMmt8nmtY4fSIbXPbGK2SQeNghBKu9d1CxlHMRqK03b0ztglPEflKDFQe2k6bpDgj600I2oe9mw1"),
     payment_method_types=["card"] ,
     mode="payment",
     customer_email,
     metadata={},
-    cancel_url=process.env.CANCEL_URL,
-    success_url=process.env.SUCCESS_URL,
+    cancel_url="http://localhost:3000/Cart",
+    success_url="http://localhost:3000",
     discounts=[],
     line_items=[]
 }={}) {
